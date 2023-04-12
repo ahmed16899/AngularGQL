583aa1634d26546a6f6a3c46473cc35567433c7b
unknown <ahmed.ayman16899@gmail.com>
2023-04-12 04:12:05 +0200

    .

--- src/app/queries.ts
+++ src/app/queries.ts
@@ -11,3 +11,79 @@ const get_books = gql`
   }
 `
 export {get_books}
+
+
+/*
+mutation{
+  deleteBook(id:"6434d2d9ae7c7e43ef8120bf")
+  {
+    id
+
+  }
+  }
+
+
+mutation{
+  AddBook(name:"c++",author:"ahmed",NumOfPages:"40")
+  {
+    name,
+author,
+NumOfPages
+
+  }
+  }
+
+
+mutation{
+  UpdateBook(id:"6434d5f099ed47b4c4f2a8ab",name:"c++++++",author:"ahmeasdasdd",NumOfPages:"46454560")
+  {
+    name,
+author,
+NumOfPages,
+    id
+
+  }
+  }
+
+
+
+
+mutation{
+  AddAuthor(name:"zzzzzz" ,age:30 ,books:["6434db79a508c46278c5830b"] )
+  {
+    name,
+age,
+books
+    {
+      name,
+      author
+    }
+
+  }
+  }
+
+
+
+mutation{
+  UpdateAuthor(id:"6434dd591affb137133ed020",name:"xxxx" ,age:30 ,books:["asdasd"] )
+  {
+    name,
+age,
+books{
+id
+}
+  
+  }
+  }
+
+
+
+mutation{
+ deleteAuthor(id:"6434dd591affb137133ed020" )
+  {
+    
+		name,
+    age,
+  }
+  }
+*/ 
